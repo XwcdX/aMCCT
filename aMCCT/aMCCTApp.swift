0681bc85d73360d26763e3c991fd984b8ad0b860
XwcdX <darkpuppet90@gmail.com>
2026-03-22 20:27:43 +0700

    Initial Commit

---
+++ aMCCT/aMCCTApp.swift
@@ -0,0 +1,17 @@
+//
+//  aMCCTApp.swift
+//  aMCCT
+//
+//  Created by Terry Clement on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct aMCCTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
