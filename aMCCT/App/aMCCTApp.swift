a2d92bfcf86228c4a60e5b1137449b0c8cacba04
Terry Clement <darkpuppet90@gmail.com>
2026-04-08 05:10:01 +0700

    chore: call new model name

--- aMCCT/App/aMCCTApp.swift
+++ aMCCT/App/aMCCTApp.swift
@@ -10,7 +10,7 @@ struct aMCCTApp: App {
 
     init() {
         do {
-            let container = try ModelContainer(for: BrainState.self, StoreItem.self, TimerRecord.self)
+            let container = try ModelContainer(for: BrainState.self, StoreItem.self, ShieldUnlockRecord.self)
             self.container = container
             self._appEnvironment = State(initialValue: AppEnvironment(modelContext: container.mainContext))
         } catch {
@@ -20,6 +20,20 @@ struct aMCCTApp: App {
 
     var body: some Scene {
         WindowGroup {
+//            TypingTaskView(
+//                            viewModel: FrictionTaskViewModel(
+//                                config: TypingTaskConfig(
+//                                    maxToken: 40,
+//                                    wordCount: 8,
+//                                    vocabulary: .advanced,
+//                                    structure: .simpleSentence,
+//                                    constraints: [.punctuationRequired]
+//                                ),
+//                                promptService: FoundationPromptService()
+//                            )
+//                        )
+//            .modelContainer(container)
+            
             DashboardView()
                 .environment(appEnvironment.dashboardViewModel)
                 .modelContainer(container)
