9773b25ef183b323c1ad033d6a38ad8c3da333b5
Axel Valerio Ertamto <axelvalerio230@gmail.com>
2026-04-08 16:36:48 +0700

    feat:make closebutton, frictioncard, progressring component

--- aMCCT/Core/Components/CloseButton.swift
+++ aMCCT/Core/Components/CloseButton.swift
@@ -1,7 +1,21 @@
-//
-//  CancelButton.swift
-//  aMCCT
-//
-//  Created by Axel Valerio Ertamto on 08/04/26.
-//
+import SwiftUI
 
+struct CloseButton: View {
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: "xmark.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(Color(UIColor.systemGray4))
+        }
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    CloseButton(action: {
+        print("Close button tapped")
+    })
+}
